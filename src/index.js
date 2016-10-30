ef80ab698d822be24636342a7db44b077f4099ee
justinl-y <j@solaris.org.uk>
2016-10-30 15:21:06 -0700

    Modified difficulty ball creation code

--- src/index.js
+++ src/index.js
@@ -38,28 +38,80 @@ let player2Score = 0;
 
 	//console.log(Math.max(player1Score, player2Score));
 
-	if ( Math.max(player1Score, player2Score) > numberOfBalls ) {
+
+	//if ( Math.max(player1Score, player2Score) > numberOfBalls ) {
 		//numberOfBalls = Math.max(player1Score, player2Score);
 
-		game.createBall(context, boardHeight, boardWidth);
-		numberOfBalls += 2;
+		//game.createBall(context, boardHeight, boardWidth);
+		//numberOfBalls += 2;
 
-		console.log(Math.max(player1Score, player2Score));
+		let playerAverage = Math.max(player1Score, player2Score);
 
-		if (Math.max(player1Score, player2Score) > 9) {
+		//console.log(playerAverage);
+
+		switch (playerAverage) {
+			case 0:
+			case 1:
+				console.log('should be 1');
+				while (game.balls.length < 1) {
+					game.createBall(context, boardHeight, boardWidth);
+				}
+				break;
+			case 2:
+			case 3:
+				console.log('should be 2');
+				while (game.balls.length < 2) {
+					game.createBall(context, boardHeight, boardWidth);
+				}
+				break;
+			case 4:
+			case 5:
+				console.log('should be 3');
+				while (game.balls.length < 3) {
+					game.createBall(context, boardHeight, boardWidth);
+				}
+				break;
+			case 6:
+			case 7:
+				console.log('should be 4');
+				while (game.balls.length < 4) {
+					game.createBall(context, boardHeight, boardWidth);
+				}
+				break;
+			case 8:
+			case 9:
+				console.log('should be 5');
+				while (game.balls.length < 5) {
+					game.createBall(context, boardHeight, boardWidth);
+				}
+				break;
+			default:
+				game.balls = [];
+				game.createBall(context, boardHeight, boardWidth);
+		}
+
+		console.log(game.balls.length);
+
+
+
+		/*if (Math.max(player1Score, player2Score) > 9) {
 			paddleColourIndex++;
+
+			//console.log(paddleColourIndex);
+
 			paddleColour = gameSettings.paddleColours[paddleColourIndex];
 
 			game.paddles = [];
 			game.createPaddle(context, boardHeight, 5, paddleColour, gameSettings.paddleWidth, gameSettings.paddleHeight, gameSettings.paddleSpeed, player1Keys);
 			game.createPaddle(context, boardHeight, boardWidth - (5 + gameSettings.paddleWidth), paddleColour, gameSettings.paddleWidth, gameSettings.paddleHeight, gameSettings.paddleSpeed, player2Keys);
-		}
+		}*/
 
 		//if (numberOfBalls > 10) {
 
 		//}
 		//console.log(numberOfBalls);
-	}
+	//}
 
+	
 	//console.log('player1Score: ' + player1Score + 'player2Score: ' + player2Score);
 }());
