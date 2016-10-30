4993886a0a5040afdc284b53343a96a302037c77
justinl-y <j@solaris.org.uk>
2016-10-29 20:23:12 -0700

    Fixed ball bug with collision on end of paddle

--- src/Ball.js
+++ src/Ball.js
@@ -12,20 +12,12 @@ export default class Ball {
         this.reset();
     }
 
-    draw(context) {
-        context.fillStyle = this.colour;
-        context.beginPath();
-        context.arc(this.x, this.y, this.radius, 0, Math.PI * 2);
-        context.fill();
-        context.closePath();
-    }
-
     //reset ball
     reset() {
         this.x = this.boardWidth / 2;
         this.y = this.boardHeight / 2;
-        this.vy = 1;
-        this.vx = 1;
+        this.vy = 1;//Math.floor(Math.random() * 12 - 6);
+        this.vx = 1; //(7 - Math.abs(this.vy));
 
         if (Math.random() > 0.5) {
             this.vx *= -1;
@@ -64,30 +56,31 @@ export default class Ball {
 
             // if past right paddle reverse x direction if paddle hit. Score if not
             if (inRightEnd) {
-                if (this.y >= paddle2.y && this.y <= (paddle2.y + paddle2.height)) {
-                    this.ballCollisionSound('paddle');
 
-                    this.vx *= -1;
+                //going up
+                if (this.vy > 0) {
+                    if ( ((this.y + this.radius) >= paddle2.y && (this.y + this.radius) <= (paddle2.y + paddle2.height)) && this.x <= paddle2.x ) {
+                        this.ballCollisionSound('paddle');
+                        this.vx *= -1;
+                    } else {
+                        if (this.x === this.boardWidth) {
+                            this.ballCollisionSound('score');
+                            p1Scoreboard.score += 1;
+                            this.reset();
+                        }
+                    }
                 } else {
-                    if (this.x === this.boardWidth) {
-                        this.ballCollisionSound('score');
-
-                        p1Scoreboard.score += 1;
-
-                        this.reset();
+                    if ( ((this.y - this.radius) >= paddle2.y && (this.y - this.radius) <= (paddle2.y + paddle2.height)) && this.x <= paddle2.x ) {
+                        this.ballCollisionSound('paddle');
+                        this.vx *= -1;
+                    } else {
+                        if (this.x === this.boardWidth) {
+                            this.ballCollisionSound('score');
+                            p1Scoreboard.score += 1;
+                            this.reset();
+                        }
                     }
                 }
-
-                /*const collisionDiff = this.x + this.boardWidth - paddle2.x;
-                const k = collisionDiff / this.vx;
-                const y = this.vy * k + (this.y - this.vy);
-                const hitRightPaddle = y >= paddle2.y && y + this.boardHeight <= paddle2.y + paddle2.height;
-
-                if (hitRightPaddle) {
-                    this.x = paddle2.x - this.boardWidth;
-                    this.y = Math.floor(this.y - this.vy + this.vy * k);
-                    this.vx = -this.vx;
-                }*/
             }
         } else {
             // going left
@@ -95,34 +88,42 @@ export default class Ball {
             // determine if ball is at or past left paddle right edge
             const inLeftEnd = (this.x - this.radius) <= paddle1.x + paddle1.width;
 
-            // if past lest paddle reverse x direction
+            // if past left paddle reverse x direction
             if (inLeftEnd) {
-                if (this.y >= paddle1.y && this.y <= (paddle1.y + paddle1.height)) {
-                    //console.log('paddle hit');
-                    this.ballCollisionSound('paddle');
-                    this.vx *= -1;
+                // going up
+                if (this.vy > 0) {
+                    if ( ((this.y + this.radius) >= paddle1.y && (this.y + this.radius) <= (paddle1.y + paddle1.height)) && this.x >= (paddle1.x + paddle1.width) ) {
+                        this.ballCollisionSound('paddle');
+                        this.vx *= -1;
+                    } else {
+                        if (this.x === 0) {
+                            this.ballCollisionSound('score');
+                            p2Scoreboard.score += 1;
+                            this.reset();
+                        }
+                    }
                 } else {
-                    if (this.x - this.radius === -4) {
-                        this.ballCollisionSound('score');
-
-                        p2Scoreboard.score += 1;
-
-                        this.reset();
+                    if ( ((this.y - this.radius) >= paddle1.y && (this.y - this.radius) <= (paddle1.y + paddle1.height)) && this.x >= (paddle1.x + paddle1.width)) {
+                        this.ballCollisionSound('paddle');
+                        this.vx *= -1;
+                    } else {
+                        if (this.x === 0) {
+                            this.ballCollisionSound('score');
+                            p2Scoreboard.score += 1;
+                            this.reset();
+                        }
                     }
                 }
-
-                /*const collisionDiff = paddle1.x + paddle1.width - this.x;
-                const k = collisionDiff / - this.vx;
-                const y = this.vy * k + (this.y - this.vy);
-                const hitLeftPaddle = y >= paddle1.y && (y + this.boardHeight) <= (paddle1.y + paddle1.height);
-
-                if (hitLeftPaddle) {
-                    this.x = paddle1.x + paddle1.width;
-                    this.y = Math.floor(this.y - this.vy + this.vy * k);
-                    this.vx = -this.vx;
-                }*/
             }
         }
+    }
+
+    draw(context) {
+        context.fillStyle = this.colour;
+        context.beginPath();
+        context.arc(this.x, this.y, this.radius, 0, Math.PI * 2);
+        context.fill();
+        context.closePath();
     }
 
     render(context, paddle1, paddle2, p1Scoreboard, p2Scoreboard) {
@@ -143,51 +144,7 @@ export default class Ball {
         this.x += this.vx;
         this.y += this.vy;
 
-        //this.score(p1Score, p2Score);
         this.paddleCollision(paddle1, paddle2, p1Scoreboard, p2Scoreboard);
         this.draw(context);
     }
 }
-
-    /*const size = 5;
-    export default class Ball {
-    constructor() {
-        this.x = 50; // random x
-        this.y = 50; // random y
-        this.vy = Math.floor(Math.random() * 12 - 6); // y direction
-        this.vx = (7 - Math.abs(this.vy)); // x direction
-        this.size = size;
-    }
-    }*/
-
-    /*paddleCollision(paddle1, paddle2) {
-        if (this.vx > 0) {
-            const inRightEnd = paddle2.x <= this.x + this.width && paddle2.x > this.x - this.vx + this.width;
-
-            if (inRightEnd) {
-                const collisionDiff = this.x + this.width - paddle2.x;
-                const k = collisionDiff / this.vx;
-                const y = this.vy * k + (this.y - this.vy);
-                const hitRightPaddle = y >= paddle2.y && y + this.height <= paddle2.y + paddle2.height;
-                if (hitRightPaddle) {
-                    this.x = paddle2.x - this.width;
-                    this.y = Math.floor(this.y - this.vy + this.vy * k);
-                    this.vx = -this.vx;
-                }
-            }
-        } else {
-            const inLeftEnd = paddle1.x + paddle1.width >= this.x;
-            if (inLeftEnd) {
-                const collisionDiff = paddle1.x + paddle1.width - this.x;
-                const k = collisionDiff / -this.vx;
-                const y = this.vy * k + (this.y - this.vy);
-                const hitLeftPaddle = y >= paddle1.y && y + this.height <= paddle1.y + paddle1.height;
-
-                if (hitLeftPaddle) {
-                    this.x = paddle1.x + paddle1.width;
-                    this.y = Math.floor(this.y - this.vy + this.vy * k);
-                    this.vx = -this.vx;
-                }
-            }
-        }
-    }*/
